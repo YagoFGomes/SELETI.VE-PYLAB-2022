d3dc6d6d892dd051110c3b86a48f0cd32e5fb4e9
Yago Ferreira <34385672+YagoFGomes@users.noreply.github.com>
2022-11-14 17:50:39 -0300

    collect statics

--- core/settings.py
+++ core/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT = os.path.join('static')
+STATIC_ROOT = os.path.join('assets')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
